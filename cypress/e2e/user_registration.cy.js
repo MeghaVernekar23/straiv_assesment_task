ee96e5ce530a92064061eee2356dac736b4c8e7d
MeghaVernekar <meghavernekar98@gmail.com>
2024-10-21 18:17:32 +0200

    User should be created when optional fields are not filled

--- cypress/e2e/user_registration.cy.js
+++ cypress/e2e/user_registration.cy.js
@@ -2,44 +2,65 @@ const url = 'https://www.automationexercise.com';
 
 describe('User Registration', () => {
     // Positive Test Case
-    it('should register a new user with proper signup form data', () => {
+    it('should register a new user with valid signup form data', () => {
         cy.visit(url)
 
         cy.get('a[href="/login"]').click()
-
         cy.get('input[data-qa="signup-name"]').type('validname');
-        cy.get('input[data-qa="signup-email"]').type('validname@example.com');
-
-        
+        cy.get('input[data-qa="signup-email"]').type('validname@example.com');        
         cy.get('button[data-qa="signup-button"]').click();
 
         cy.get('#id_gender1').check()
-
         cy.get('input[data-qa="password"]').type('password123')
-
         cy.get('select[data-qa="days"]').select('10')  
         cy.get('select[data-qa="months"]').select('January')
         cy.get('select[data-qa="years"]').select('1990')
-
         cy.get('input[data-qa="first_name"]').type('John')
         cy.get('input[data-qa="last_name"]').type('Doe')
         cy.get('input[data-qa="company"]').type('Test Company')
         cy.get('input[data-qa="address"]').type('123 Test Street')
-
         cy.get('select[data-qa="country"]').select('Australia')
-
         cy.get('input[data-qa="state"]').type('California')
         cy.get('input[data-qa="city"]').type('City_1')
         cy.get('input[data-qa="zipcode"]').type('90210')
         cy.get('input[data-qa="mobile_number"]').type('1234567890')
 
         cy.get('button[data-qa="create-account"]').click()
-
         cy.contains('Account Created!').should('be.visible')
         cy.get('a[data-qa="continue-button"]').click();
         cy.get('a[href="/delete_account"]').click()
         cy.get('a[data-qa="continue-button"]').click();
     })
+
+    it('should register a new user with optional fields not filled', () => {
+        cy.visit(url)
+
+        cy.get('a[href="/login"]').click()
+        cy.get('input[data-qa="signup-name"]').type('validname');
+        cy.get('input[data-qa="signup-email"]').type('validname@example.com');        
+        cy.get('button[data-qa="signup-button"]').click();
+
+        // cy.get('#id_gender1').check()
+        cy.get('input[data-qa="password"]').type('password123')
+        cy.get('select[data-qa="days"]').select('10')  
+        cy.get('select[data-qa="months"]').select('January')
+        cy.get('select[data-qa="years"]').select('1990')
+        cy.get('input[data-qa="first_name"]').type('John')
+        cy.get('input[data-qa="last_name"]').type('Doe')
+        // cy.get('input[data-qa="company"]').type('Test Company')
+        cy.get('input[data-qa="address"]').type('123 Test Street')
+        cy.get('select[data-qa="country"]').select('Australia')
+        cy.get('input[data-qa="state"]').type('California')
+        cy.get('input[data-qa="city"]').type('City_1')
+        cy.get('input[data-qa="zipcode"]').type('90210')
+        cy.get('input[data-qa="mobile_number"]').type('1234567890')
+
+        cy.get('button[data-qa="create-account"]').click()
+        cy.contains('Account Created!').should('be.visible')
+        cy.get('a[data-qa="continue-button"]').click();
+        cy.get('a[href="/delete_account"]').click()
+        cy.get('a[data-qa="continue-button"]').click();
+    });
 
     // Negative Test Case
     it('should not be allowed to register new user as validation fails', () => {
