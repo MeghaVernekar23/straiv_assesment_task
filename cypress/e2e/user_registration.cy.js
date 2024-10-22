44cf46bbc6cfa8b3df30120f58be8b5249ca296a
MeghaVernekar <meghavernekar98@gmail.com>
2024-10-22 09:24:26 +0200

    Removed unwanted blank spaces

--- cypress/e2e/user_registration.cy.js
+++ cypress/e2e/user_registration.cy.js
@@ -5,17 +5,13 @@ describe('User Registration', () => {
     // Positive Test Case
     it('should register a new user with valid signup form data', () => {
         navigateToLogin();
-
         registerUser('validname', 'validname@example.com')
-        
         deleteUserAccount();
     })
 
     it('should register a new user with optional fields not filled', () => {
         navigateToLogin();
-
         registerUser('validname', 'validname@example.com', false)
-
         deleteUserAccount();
     });
 
@@ -27,13 +23,9 @@ describe('User Registration', () => {
 
     it('should show error for already registered email', () => {
         navigateToLogin();
-
         registerUser('validname', 'validname@example.com');
-
         logoutUser();
-
         fillSignUpForm('validname', 'validname@example.com')
-        
         cy.get('.signup-form').should('contain.text', 'Email Address already exist!');
         
     })
@@ -41,14 +33,10 @@ describe('User Registration', () => {
     // Negative Test Case
     it('should not be allowed to register new user as validation fails', () => {
         navigateToLogin();
-
         fillSignUpForm('invalidname', 'invaliduser4-example.com')
-
         cy.get('input[data-qa="signup-email"]').then(($input) => {
             expect($input[0].checkValidity()).to.be.false;   
             expect($input[0].validationMessage).to.eq('Die E-Mail-Adresse muss ein @-Zeichen enthalten. In der Angabe "invaliduser4-example.com" fehlt ein @-Zeichen.');
         });
-
     });
-
 })
