2a749a60216b48d900b76cb0abed48231d7886c2
MeghaVernekar <meghavernekar98@gmail.com>
2024-10-22 09:10:46 +0200

    User registration module refactored with helper functions

--- cypress/e2e/user_registration.cy.js
+++ cypress/e2e/user_registration.cy.js
@@ -1,30 +1,12 @@
-import { deleteUserAccount, Url, navigateToLogin, fillSignupForm } from "../support/userRegHelpers";
+import { deleteUserAccount, fillSignUpForm, navigateToLogin, registerUser, logoutUser } from "../support/userRegHelpers";
 
 describe('User Registration', () => {
+
     // Positive Test Case
     it('should register a new user with valid signup form data', () => {
         navigateToLogin();
 
-        fillSignupForm('validname', 'validname@example.com')
-
-        cy.get('#id_gender1').check()
-        cy.get('input[data-qa="password"]').type('password123')
-        cy.get('select[data-qa="days"]').select('10')  
-        cy.get('select[data-qa="months"]').select('January')
-        cy.get('select[data-qa="years"]').select('1990')
-        cy.get('input[data-qa="first_name"]').type('John')
-        cy.get('input[data-qa="last_name"]').type('Doe')
-        cy.get('input[data-qa="company"]').type('Test Company')
-        cy.get('input[data-qa="address"]').type('123 Test Street')
-        cy.get('select[data-qa="country"]').select('Australia')
-        cy.get('input[data-qa="state"]').type('California')
-        cy.get('input[data-qa="city"]').type('City_1')
-        cy.get('input[data-qa="zipcode"]').type('90210')
-        cy.get('input[data-qa="mobile_number"]').type('1234567890')
-
-        cy.get('button[data-qa="create-account"]').click()
-        cy.contains('Account Created!').should('be.visible')
-        cy.get('a[data-qa="continue-button"]').click();
+        registerUser('validname', 'validname@example.com')
         
         deleteUserAccount();
     })
@@ -32,84 +14,26 @@ describe('User Registration', () => {
     it('should register a new user with optional fields not filled', () => {
         navigateToLogin();
 
-        fillSignupForm('validname', 'validname@example.com')
+        registerUser('validname', 'validname@example.com', false)
 
-        // cy.get('#id_gender1').check()
-        cy.get('input[data-qa="password"]').type('password123')
-        cy.get('select[data-qa="days"]').select('10')  
-        cy.get('select[data-qa="months"]').select('January')
-        cy.get('select[data-qa="years"]').select('1990')
-        cy.get('input[data-qa="first_name"]').type('John')
-        cy.get('input[data-qa="last_name"]').type('Doe')
-        // cy.get('input[data-qa="company"]').type('Test Company')
-        cy.get('input[data-qa="address"]').type('123 Test Street')
-        cy.get('select[data-qa="country"]').select('Australia')
-        cy.get('input[data-qa="state"]').type('California')
-        cy.get('input[data-qa="city"]').type('City_1')
-        cy.get('input[data-qa="zipcode"]').type('90210')
-        cy.get('input[data-qa="mobile_number"]').type('1234567890')
-
-        cy.get('button[data-qa="create-account"]').click()
-        cy.contains('Account Created!').should('be.visible')
-        cy.get('a[data-qa="continue-button"]').click();
-        
         deleteUserAccount();
     });
 
     it('should register a new user with case-insensitive data', () => {
         navigateToLogin();
-
-        fillSignupForm('VALIDNAME', 'VALID.NAME@example.com')
-
-        cy.get('#id_gender1').check()
-        cy.get('input[data-qa="password"]').type('password123')
-        cy.get('select[data-qa="days"]').select('10')  
-        cy.get('select[data-qa="months"]').select('January')
-        cy.get('select[data-qa="years"]').select('1990')
-        cy.get('input[data-qa="first_name"]').type('John')
-        cy.get('input[data-qa="last_name"]').type('Doe')
-        cy.get('input[data-qa="company"]').type('Test Company')
-        cy.get('input[data-qa="address"]').type('123 Test Street')
-        cy.get('select[data-qa="country"]').select('Australia')
-        cy.get('input[data-qa="state"]').type('California')
-        cy.get('input[data-qa="city"]').type('City_1')
-        cy.get('input[data-qa="zipcode"]').type('90210')
-        cy.get('input[data-qa="mobile_number"]').type('1234567890')
-
-        cy.get('button[data-qa="create-account"]').click()
-        cy.contains('Account Created!').should('be.visible')
-        cy.get('a[data-qa="continue-button"]').click();
-        
+        registerUser('VALIDNAME', 'VALID.NAME@example.com') 
         deleteUserAccount();
     });
 
     it('should show error for already registered email', () => {
         navigateToLogin();
 
-        fillSignupForm('validname', 'validname@example.com')
+        registerUser('validname', 'validname@example.com');
 
-        cy.get('#id_gender1').check()
-        cy.get('input[data-qa="password"]').type('password123')
-        cy.get('select[data-qa="days"]').select('10')  
-        cy.get('select[data-qa="months"]').select('January')
-        cy.get('select[data-qa="years"]').select('1990')
-        cy.get('input[data-qa="first_name"]').type('John')
-        cy.get('input[data-qa="last_name"]').type('Doe')
-        cy.get('input[data-qa="company"]').type('Test Company')
-        cy.get('input[data-qa="address"]').type('123 Test Street')
-        cy.get('select[data-qa="country"]').select('Australia')
-        cy.get('input[data-qa="state"]').type('California')
-        cy.get('input[data-qa="city"]').type('City_1')
-        cy.get('input[data-qa="zipcode"]').type('90210')
-        cy.get('input[data-qa="mobile_number"]').type('1234567890')
+        logoutUser();
 
-        cy.get('button[data-qa="create-account"]').click()
-        cy.contains('Account Created!').should('be.visible')
-        cy.get('a[data-qa="continue-button"]').click();
-        cy.get('a[href="/logout"]').click()
-        cy.get('input[data-qa="signup-name"]').type('validname');
-        cy.get('input[data-qa="signup-email"]').type('validname@example.com');
-        cy.get('button[data-qa="signup-button"]').click(); 
+        fillSignUpForm('validname', 'validname@example.com')
+        
         cy.get('.signup-form').should('contain.text', 'Email Address already exist!');
         
     })
@@ -118,7 +42,7 @@ describe('User Registration', () => {
     it('should not be allowed to register new user as validation fails', () => {
         navigateToLogin();
 
-        fillSignupForm('invalidname', 'invaliduser4-example.com')
+        fillSignUpForm('invalidname', 'invaliduser4-example.com')
 
         cy.get('input[data-qa="signup-email"]').then(($input) => {
             expect($input[0].checkValidity()).to.be.false;   
--- cypress/support/userRegHelpers.js
+++ cypress/support/userRegHelpers.js
@@ -1,17 +1,94 @@
+const generateRandomString = (length) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
+
+const generateRandomEmail = () => {
+    const randomString = generateRandomString(10);
+    return `${randomString}@example.com`;
+};
+
+const generateRandomNumber = (length) => {
+    let result = '';
+    const characters = '0123456789';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
+
+const generateRandomDate = () => {
+    const day = Math.floor(Math.random() * 28) + 1; 
+    const month = Math.floor(Math.random() * 12) + 1;
+    const year = Math.floor(Math.random() * 30) + 1980;
+    return { day, month, year };
+};
+
 export const Url = 'https://www.automationexercise.com';
 
 export const deleteUserAccount = () => {
+    /** Deletes user account */
+
     cy.get('a[href="/delete_account"]').click();
     cy.get('a[data-qa="continue-button"]').click();
 };
 
 export const navigateToLogin = () => {
+    /** Navigates to the signup/login page */
     cy.visit(Url);
     cy.get('a[href="/login"]').click();
 };
 
-export const fillSignupForm = (username, useremail) => {
+export const registerUser = (username, useremail, include_optional_fields=true) => {
+    /** Creates a new account for a unique user */
+
+    // const randomName = generateRandomString(8);
+    // const randomEmail = generateRandomEmail();
+    const randomPassword = generateRandomString(12);
+    const randomMobile = generateRandomNumber(10);
+    const randomDate = generateRandomDate();
+
     cy.get('input[data-qa="signup-name"]').type(username);
     cy.get('input[data-qa="signup-email"]').type(useremail);        
     cy.get('button[data-qa="signup-button"]').click();
+
+    if (include_optional_fields == true){
+        cy.get('#id_gender1').check();
+        cy.get('input[data-qa="company"]').type('Test Company');
+    }
+    cy.get('input[data-qa="password"]').type(randomPassword);
+    cy.get('select[data-qa="days"]').select(randomDate.day.toString());
+    cy.get('select[data-qa="months"]').select(randomDate.month.toString());
+    cy.get('select[data-qa="years"]').select(randomDate.year.toString());
+    cy.get('input[data-qa="first_name"]').type('John');
+    cy.get('input[data-qa="last_name"]').type('Doe');
+    cy.get('input[data-qa="company"]').type('Test Company');
+    cy.get('input[data-qa="address"]').type('123 Test Street');
+    cy.get('select[data-qa="country"]').select('Australia');
+    cy.get('input[data-qa="state"]').type('Sydney');
+    cy.get('input[data-qa="city"]').type('City_1');
+    cy.get('input[data-qa="zipcode"]').type('90210');
+    cy.get('input[data-qa="mobile_number"]').type(randomMobile);
+
+    cy.get('button[data-qa="create-account"]').click();
+    cy.contains('Account Created!').should('be.visible');
+    cy.get('a[data-qa="continue-button"]').click();
 };
+
+export const fillSignUpForm = (username, useremail) => {
+    /** Fills the signup form before the user account form is created */
+
+    cy.get('input[data-qa="signup-name"]').type(username);
+    cy.get('input[data-qa="signup-email"]').type(useremail);        
+    cy.get('button[data-qa="signup-button"]').click();
+}
+
+export const logoutUser = () => {
+    /** Logs out the user */
+    
+    cy.get('a[href="/logout"]').click();
+}
