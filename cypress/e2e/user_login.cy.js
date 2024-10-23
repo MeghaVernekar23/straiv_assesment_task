1ac32376a6e69a1be918437ab073ab38818b8760
MeghaVernekar <meghavernekar98@gmail.com>
2024-10-23 10:13:10 +0200

    Reorganised testcases

--- cypress/e2e/user_login.cy.js
+++ cypress/e2e/user_login.cy.js
@@ -5,17 +5,17 @@ import { logInUser } from "../support/userLoginHelpers";
 describe('User Login with Positive Scenarios', () => {
 
   // Positive test case
+
+  it('Should be able to see the login form', () => {
+    cy.navigatelogin();
+  });
+
   it('Should be able to allow login with proper credentials', () => {
     cy.navigatelogin();
     registerUser(userName, userEmail);
     cy.logout();
     logInUser(userEmail, userPassword, loginUrl);
     cy.deleteUserAccount();
-  });
-
-  
-  it('Should be able to see the login form', () => {
-    cy.navigatelogin();
   });
 
   it('should display an error for invalid credentials', () => {
@@ -26,11 +26,12 @@ describe('User Login with Positive Scenarios', () => {
       cy.get('button[data-qa="login-button"]').click();
       cy.get('.login-form').should('contain.text', 'Your email or password is incorrect!');
   });
+
 });
 
 describe('User Login with Negative Scenarios', () => {
-  it('should prevent SQL injection in login form', () => {
 
+  it('should prevent SQL injection in login form', () => {
     cy.visit(loginUrl);
     cy.get('input[data-qa="login-email"]').type("OR **--");
     cy.get('input[data-qa="login-password"]').type('password');
@@ -38,6 +39,6 @@ describe('User Login with Negative Scenarios', () => {
     cy.get('input[data-qa="login-email"]').then(($input) => {
         expect($input[0].checkValidity()).to.be.false;   
         expect($input[0].validationMessage).to.eq('Die E-Mail-Adresse muss ein @-Zeichen enthalten. In der Angabe "OR**--" fehlt ein @-Zeichen.');
-      });
+    });
   });
 });
