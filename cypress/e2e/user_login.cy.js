f4b3ce62f65e9d9cf3d22312e8f127748dbae08c
MeghaVernekar <meghavernekar98@gmail.com>
2024-10-23 10:10:58 +0200

    Removed unnecessary spaces

--- cypress/e2e/user_login.cy.js
+++ cypress/e2e/user_login.cy.js
@@ -2,8 +2,6 @@ import { registerUser } from "../support/userRegHelpers";
 import { userName, userEmail, loginUrl, userPassword } from "../support/commonHelpers";
 import { logInUser } from "../support/userLoginHelpers";
 
-// const loginUrl = 'https://www.automationexercise.com/login';
-
 describe('User Login', () => {
 
   // Positive test case
@@ -12,9 +10,8 @@ describe('User Login', () => {
     registerUser(userName, userEmail);
     cy.logout();
     logInUser(userEmail, userPassword, loginUrl);
-    cy.deleteUserAccount();;
-
-});
+    cy.deleteUserAccount();
+  });
 
   
   it('Should be able to see the login form', () => {
@@ -24,12 +21,9 @@ describe('User Login', () => {
   it('should display an error for invalid credentials', () => {
 
       cy.navigatelogin();
-
       cy.get('input[data-qa="login-email"]').type('invaliduser@example.com');
       cy.get('input[data-qa="login-password"]').type('invalidpassword');
-
       cy.get('button[data-qa="login-button"]').click();
-
       cy.get('.login-form').should('contain.text', 'Your email or password is incorrect!');
   });
 });
@@ -38,14 +32,9 @@ describe('SQL Injection Test', () => {
   it('should prevent SQL injection in login form', () => {
 
     cy.visit(loginUrl);
-    // cy.get('form').then(form => {
-    //   form[0].setAttribute('novalidate', 'novalidate');
-    // });
     cy.get('input[data-qa="login-email"]').type("OR **--");
     cy.get('input[data-qa="login-password"]').type('password');
-
     cy.get('button[data-qa="login-button"]').click();
-    
     cy.get('input[data-qa="login-email"]').then(($input) => {
         expect($input[0].checkValidity()).to.be.false;   
         expect($input[0].validationMessage).to.eq('Die E-Mail-Adresse muss ein @-Zeichen enthalten. In der Angabe "OR**--" fehlt ein @-Zeichen.');
