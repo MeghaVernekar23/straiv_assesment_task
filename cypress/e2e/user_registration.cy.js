ad3dfccdba0087feb163df074ee9ad5054a16159
MeghaVernekar <meghavernekar98@gmail.com>
2024-10-24 09:51:54 +0200

    Created helpful classes for user registration

--- cypress/e2e/user_registration.cy.js
+++ cypress/e2e/user_registration.cy.js
@@ -1,4 +1,4 @@
-import { fillSignUpForm, registerUser } from "../support/userRegistrationHelpers";
+import { fillSignUpForm, registerUser, UserRegistration } from "../support/userRegistrationHelpers";
 import { user } from "../support/commonHelpers";
 import { logInUser } from "../support/userLoginHelpers";
 
@@ -6,28 +6,30 @@ beforeEach(() => {
     cy.navigatelogin();
 });
 
+const userRegistration = new UserRegistration();
+
 describe('User Registration with positive scenarios', () => {
 
     it('should register a new user with valid signup form data', () => {
         cy.wait(1000);
-        registerUser(user.name, user.email)
+        userRegistration.registerUser(user.name, user.email)
         cy.deleteUserAccount();
     })
 
     it('should register a new user with optional fields not filled', () => {
-        registerUser(user.name, user.email, false)
+        userRegistration.registerUser(user.name, user.email, false)
         cy.deleteUserAccount();
     });
 
     it('should register a new user with case-insensitive data', () => {
-        registerUser('VALIDNAME', 'VALID.NAME@example.com', true) 
+        userRegistration.registerUser('VALIDNAME', 'VALID.NAME@example.com', true) 
         cy.deleteUserAccount();
     });
 
     it('should show error for already registered email', () => {
-        registerUser(user.name, user.email);
+        userRegistration.registerUser(user.name, user.email);
         cy.logout();
-        fillSignUpForm(user.name, user.email)
+        userRegistration.fillSignUpForm(user.name, user.email)
         cy.get('.signup-form').should('contain.text', 'Email Address already exist!');
         cy.navigatelogin();
         logInUser(user.email, user.password);
@@ -38,7 +40,7 @@ describe('User Registration with positive scenarios', () => {
 describe('User Registration with negative scenarios', () => {
 
     it('should not be allowed to register new user as validation fails', () => {
-        fillSignUpForm('invalidname', 'invaliduser4-example.com')
+        userRegistration.fillSignUpForm('invalidname', 'invaliduser4-example.com')
         cy.get('input[data-qa="signup-email"]').then(($input) => {
             expect($input[0].checkValidity()).to.be.false;   
             expect($input[0].validationMessage).to.eq('Die E-Mail-Adresse muss ein @-Zeichen enthalten. In der Angabe "invaliduser4-example.com" fehlt ein @-Zeichen.');
@@ -46,7 +48,7 @@ describe('User Registration with negative scenarios', () => {
     });
     
     it('should not be allowed to register new user as there is missing data in mandatory fields', () => {
-        fillSignUpForm(user.name, user.email);
+        userRegistration.fillSignUpForm(user.name, user.email);
         cy.get('button[data-qa="create-account"]').click();
         cy.get('input[data-qa="password"]').then(($input) => {
             expect($input[0].checkValidity()).to.be.false;   
@@ -55,7 +57,7 @@ describe('User Registration with negative scenarios', () => {
     });
 
     it('should not be allowed to register new user as there is missing firstname mandatory field', () => {
-        fillSignUpForm(user.name, user.email);
+        userRegistration.fillSignUpForm(user.name, user.email);
         cy.get('button[data-qa="create-account"]').click();
         cy.get('input[data-qa="password"]').type("password123");
         cy.get('input[data-qa="first_name"]').then(($input) => {
--- cypress/support/userRegistrationHelpers.js
+++ cypress/support/userRegistrationHelpers.js
@@ -18,52 +18,54 @@ const generateRandomDate = () => {
     return { day, month, year };
 };
 
-export const registerUser = (userName, userEmail, include_optional_fields=true) => {
-    /** Creates a new account for a unique user */
 
-    const randomMobile = generateRandomNumber(10);
-    const randomDate = generateRandomDate();
+export class UserRegistration {
 
-    cy.get('input[data-qa="signup-name"]').type(userName);
-    cy.get('input[data-qa="signup-email"]').type(userEmail);        
-    cy.get('button[data-qa="signup-button"]').click();
-
-    cy.wait(1000);
-    cy.contains('Enter Account Information').should('be.visible');
-
-    if (include_optional_fields == true){
-        cy.get('#id_gender1').check();
-        cy.get('input[data-qa="company"]').type('Test Company');
+    fillSignUpForm (username, useremail) {
+        /** Fills the signup form before the user account form is created */
+    
+        cy.get('input[data-qa="signup-name"]').type(username);
+        cy.get('input[data-qa="signup-email"]').type(useremail); 
+        cy.get('button[data-qa="signup-button"]').click();
     }
-    cy.get('input[data-qa="password"]').type("password123");
-    cy.get('select[data-qa="days"]').select(randomDate.day.toString());
-    cy.get('select[data-qa="months"]').select(randomDate.month.toString());
-    cy.get('select[data-qa="years"]').select(randomDate.year.toString());
 
-    cy.get('input[id="newsletter"]').check();
-    cy.get('input[id="optin"]').check();
-    cy.get('input[data-qa="first_name"]').type('John');
-    cy.get('input[data-qa="last_name"]').type('Doe');
-    cy.get('input[data-qa="company"]').type('Test Company');
-    cy.get('input[data-qa="address"]').type('123 Test Street');
-    cy.get('select[data-qa="country"]').select('Australia');
-    cy.get('input[data-qa="state"]').type('Sydney');
-    cy.get('input[data-qa="city"]').type('City_1');
-    cy.get('input[data-qa="zipcode"]').type('90210');
-    cy.get('input[data-qa="mobile_number"]').type(randomMobile);
-
-    cy.get('button[data-qa="create-account"]').click();
-    cy.wait(1000);
-    cy.contains('Account Created!').should('be.visible');
-    cy.get('a[data-qa="continue-button"]').click();
-    cy.wait(1000);
-    cy.contains(`Logged in as ${userName}`).should('be.visible');
-};
-
-export const fillSignUpForm = (username, useremail) => {
-    /** Fills the signup form before the user account form is created */
-
-    cy.get('input[data-qa="signup-name"]').type(username);
-    cy.get('input[data-qa="signup-email"]').type(useremail); 
-    cy.get('button[data-qa="signup-button"]').click();
+    registerUser (userName, userEmail, include_optional_fields=true) {
+        /** Creates a new account for a unique user */
+    
+        const randomMobile = generateRandomNumber(10);
+        const randomDate = generateRandomDate();
+        
+        this.fillSignUpForm(userName, userEmail)
+    
+        cy.wait(1000);
+        cy.contains('Enter Account Information').should('be.visible');
+    
+        if (include_optional_fields == true){
+            cy.get('#id_gender1').check();
+            cy.get('input[data-qa="company"]').type('Test Company');
+        }
+        cy.get('input[data-qa="password"]').type("password123");
+        cy.get('select[data-qa="days"]').select(randomDate.day.toString());
+        cy.get('select[data-qa="months"]').select(randomDate.month.toString());
+        cy.get('select[data-qa="years"]').select(randomDate.year.toString());
+    
+        cy.get('input[id="newsletter"]').check();
+        cy.get('input[id="optin"]').check();
+        cy.get('input[data-qa="first_name"]').type('John');
+        cy.get('input[data-qa="last_name"]').type('Doe');
+        cy.get('input[data-qa="company"]').type('Test Company');
+        cy.get('input[data-qa="address"]').type('123 Test Street');
+        cy.get('select[data-qa="country"]').select('Australia');
+        cy.get('input[data-qa="state"]').type('Sydney');
+        cy.get('input[data-qa="city"]').type('City_1');
+        cy.get('input[data-qa="zipcode"]').type('90210');
+        cy.get('input[data-qa="mobile_number"]').type(randomMobile);
+    
+        cy.get('button[data-qa="create-account"]').click();
+        cy.wait(1000);
+        cy.contains('Account Created!').should('be.visible');
+        cy.get('a[data-qa="continue-button"]').click();
+        cy.wait(1000);
+        cy.contains(`Logged in as ${userName}`).should('be.visible');
+    };
 }
